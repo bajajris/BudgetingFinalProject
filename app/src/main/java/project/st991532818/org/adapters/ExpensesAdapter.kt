55b356f5f3c1a886823bd43360a18bed2c996df1
rajput0 <rajput0@sheridancollege.ca>
2021-12-01 06:02:01 -0500

    Added recycler view and cardview to display expenses. Created Expenses List adapter.

---
+++ app/src/main/java/project/st991532818/org/adapters/ExpensesAdapter.kt
@@ -0,0 +1,49 @@
+package project.st991532818.org.adapters
+
+import android.R
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import project.st991532818.org.databinding.ListItemExpenseBinding
+import project.st991532818.org.model.expenses.Expense
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.*
+import kotlin.collections.ArrayList
+
+class ExpensesAdapter(private val context: Context, private val dataSource: ArrayList<Expense>) : RecyclerView.Adapter<ExpensesAdapter.VH>() {
+
+
+    private lateinit var binding: ListItemExpenseBinding
+    class VH(binding: ListItemExpenseBinding) : RecyclerView.ViewHolder(binding.root){
+        var expenseTypeLabel = binding.expenseTypeLabel
+        var amount = binding.amount
+        var category = binding.category
+        var dateTime = binding.dateTime
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        binding = ListItemExpenseBinding.inflate(LayoutInflater.from(parent.context))
+        return VH(binding)
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        val e = dataSource[position]
+        var col:Int = 0;
+        col = R.color.holo_red_light
+
+        holder.expenseTypeLabel.background = context.getDrawable(col)
+        holder.amount.text = e.amount.toString()
+        holder.category.text = e.category
+//        val dateTime : String = LocalDateTime.parse(e.createdAt, DateTimeFormatter.ISO_OFFSET_DATE_TIME).format(
+//            DateTimeFormatter.ofLocalizedDateTime(
+//            FormatStyle.LONG, FormatStyle.SHORT)).toString()
+
+        holder.dateTime.text = (e.month + e.year)// todo
+    }
+
+    override fun getItemCount() = dataSource.size
+
+}
--- app/src/main/java/project/st991532818/org/ui/home/HomeFragment.kt
+++ app/src/main/java/project/st991532818/org/ui/home/HomeFragment.kt
@@ -1,17 +1,26 @@
 package project.st991532818.org.ui.home
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import project.st991532818.org.R
 import project.st991532818.org.databinding.FragmentHomeBinding
+import project.st991532818.org.model.expenses.Expense
+import project.st991532818.org.adapters.ExpensesAdapter
+import com.google.firebase.firestore.*
 
 class HomeFragment : Fragment() {
+
+    val fireStoreDatabase = FirebaseFirestore.getInstance()
+    private var _expensesList = ArrayList<Expense>()
+    private lateinit var _adapter : ExpensesAdapter
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
@@ -20,6 +29,7 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -31,11 +41,47 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
+        _adapter = ExpensesAdapter(requireContext(),_expensesList)
+        // create recycler view
+        val recyclerView = root.findViewById<RecyclerView>(R.id.expenseRecyclerView)
+        // attach the adapter to recycler view
+        recyclerView.adapter = _adapter
+
+
+        recyclerView.setHasFixedSize(true)
+        val llm = LinearLayoutManager(context)
+        llm.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.setLayoutManager(llm)
+        readData()
+        _adapter.notifyDataSetChanged()
+        _adapter.notifyDataSetChanged()
+
         return root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun readData(){
+        fireStoreDatabase.collection("expenses")
+            .get()
+            .addOnCompleteListener {
+                val result : StringBuffer = StringBuffer()
+                if(it.isSuccessful){
+                    for(document in it.result!!) {
+                        val amount = document.data.getValue("amount").toString().toDouble()
+                        val category = document.data.getValue("category").toString()
+                        val month = document.data.getValue("month").toString()+" "
+                        val year = document.data.getValue("year").toString().toInt()
+                        val e = Expense(amount, category, month, year)
+                        _expensesList.add(e)
+                        _adapter.notifyDataSetChanged()
+                    }
+                }
+            }
     }
 
     override fun onDestroyView() {
