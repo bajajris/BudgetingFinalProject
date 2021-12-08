11e776eccc2a380b7ab28ac7b4fdc6019e541c31
Rishabh Bajaj <bajajris@shernet.sheridancollege.ca>
2021-12-08 11:42:49 -0500

    scroll view on budget fragment

--- app/src/main/java/project/st991532818/org/models/Budget.kt
+++ app/src/main/java/project/st991532818/org/models/Budget.kt
@@ -1,5 +1,10 @@
 package project.st991532818.org.models
 
+/**
+ * Author: Rishabh Bajaj
+ *
+ */
+
 class Budget (amount: Double = 0.0, month: String = "", year: Long = 0) {
     var id: String = "";
     var amount: Double = 0.0
